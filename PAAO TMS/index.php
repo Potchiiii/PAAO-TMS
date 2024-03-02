96c4822a3379849ec2127961826006f15f26d8ed
Daud Hassan <96364732+Potchiiii@users.noreply.github.com>
2024-03-02 11:28:18 +0800

    Add files via upload

---
+++ PAAO TMS/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login Page</title>
+</head>
+<body style="background-image: url('Images/PAAO logo.jpg'); font-family: 'Arial', sans-serif; margin: 0; padding: 0;">
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="#" method="post">
+            <div class="input-group">
+                <label for="username">Employee ID</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Login</button>
+            <a href="adminLogIn.html">ADMIN LOGIN</a>
+        </form>
+    </div>
+</body>
+</html>
